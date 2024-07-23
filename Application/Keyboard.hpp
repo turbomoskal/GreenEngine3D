d406bcc01e02a43b525bf67cabea192d9cbb173c
turbomoskal <einhorn.sv@gmail.com>
2024-07-23 23:29:42 +0300

    коды клавиш

---
+++ Application/Keyboard.hpp
@@ -0,0 +1,62 @@
+﻿/*
+*		Keyboard codes
+*/
+#pragma once
+
+enum Keyboard
+{
+	None = 0x00,	// Nothing is pressed
+	LButton,		// Left mouse
+	RButton,		// Right mouse
+	Cancel,			// ?Cancel
+	MButton,		// Middle mouse
+	XButton1,		// ?X1 mouse
+	XButton2,		// ?X2 mouse
+	Backspace = 0x08,
+	Tab,
+	Clear = 0x0c,
+	Enter,
+	Shift = 0x10,
+	Ctrl,
+	Alt,
+	Pause,
+	Caps,
+	Esc = 0x1b,
+	Space = 0x20,
+	PgUp,
+	PgDown,
+	End,
+	Home,
+	Left,
+	Up,
+	Right,
+	Down,
+	Insert = 0x2d,
+	Delete,
+	A = 0x41,
+	B,
+	C,
+	D,
+	E,
+	F,
+	G,
+	H,
+	I,
+	J,
+	K,
+	L,
+	M,
+	N,
+	O,
+	P,
+	Q,
+	R,
+	S,
+	T,
+	U,
+	V,
+	W,
+	X,
+	Y,
+	Z
+};
