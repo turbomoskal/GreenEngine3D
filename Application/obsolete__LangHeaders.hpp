c11fc30f7bf156df7c2bebe582243cd40774aed5
Arseniy <einhorn.sv@gmail.com>
2024-08-31 01:15:12 +0300

    Сброшено устаревшее говно от R-Wolf

---
+++ Application/obsolete__LangHeaders.hpp
@@ -0,0 +1,13 @@
+﻿/*
+*		C++ STD headers
+*/
+
+#pragma once
+#include <thread>
+#include <chrono>
+#include <memory>
+#include <string>
+#include <map>
+#include <cassert>
+#include <algorithm>
+#include <queue>
